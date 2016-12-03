a2d86877c36aa2bd0c10d6331c5eac7342c9d287
sevic6 <zeljkosevic95@gmail.com>
2016-12-03 23:48:52 +0100

    ADD: Crawler beta

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 var express=require("express");
 var app=express();
-var requestify=require("requestify");
-
+var request=require("sync-request");
 
 var toCrawl=["http://localbyteout.com"];
 var crawled={};
 
-var urlRegex=new RegExp(/<a href="(http(s)?:\/\/)?([^"]*)"\s*(target="_blank")?>(.*?)<\/a>/g);
+var urlRegex=new RegExp(/<a href="(.*?)\/?".*>(.*?)<\/a>/g);
 
 function crawling(url){
-	if(url in crawled){
-		console.log("no");
-	}else{
-		requestify.get(url).then(function(response) {
+
+  var brojiteracija = 0
+	while(toCrawl.length>0)
+	{ 
+		var response=request('get',toCrawl[0].trim());
+		response=response.getBody();
 			var matches;
-			while(matches=urlRegex.exec(response.body)){
-				toCrawl.push(matches[3]);
-				console.log(matches[3]);
-			}
-			crawled[toCrawl.shift()]=true;
-		});
+			while(matches=urlRegex.exec(response)){
+				var sajt = matches[1];
+				if(matches[1].indexOf("http://") == 0)
+				{
+					if(!(crawled[matches[1]] == true || (toCrawl.indexOf(matches[1])>-1) || matches[1].indexOf('#') != -1))
+					{
+						toCrawl.push(matches[1]);
+					//	console.log("Dodat u niz: " + matches[1]);
+				//		console.log(toCrawl);
+					}
+				}
+				else if(matches[1].indexOf("https://") == -1)
+				{
+					sajt = toCrawl[0] +"/" + matches[1];
+					if(!(crawled[sajt] == true || (toCrawl.indexOf(sajt)>-1) || sajt.indexOf('#') != -1))
+					{
+						toCrawl.push(sajt);
+			//			console.log("Dodat u niz: " + sajt);
+		//				console.log(toCrawl);
+					}
+				}
+		}
+			crawled[toCrawl[0]]=true;
+			brojiteracija++;
+			toCrawl.shift();
+			console.log(brojiteracija + "*************************");
+			console.log(toCrawl);
+			console.log(crawled);
+   	//	console.log(brojiteracija);
+
 	}
 }
 
