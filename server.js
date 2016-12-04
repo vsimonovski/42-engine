59ba2b92b321f39020ca4345f69321907714f237
Vladimir Simonovski <vlsim@outlook.com>
2016-12-04 02:04:27 +0100

    FIX: Readability

--- server.js
+++ server.js
@@ -14,36 +14,39 @@ function getPosition(string, char, index) {
 }
 
 function crawling(url){
+var brojiteracija = 0;
 
-  var brojiteracija = 0
-	while(toCrawl.length>0)
-	{ 
-		var response=request('get',toCrawl[0]);
-		response=response.getBody();
-			var matches;			
-			linkscrape(toCrawl[0],response,function(links,$){
-				var i=0;
-				while(i<links.length){
-					var link=links[i].link;
-					if(link!==null && link.indexOf("https://")===-1){
-						if(!(crawled[link] == true || (toCrawl.indexOf(link)>-1) || link.indexOf('#') != -1)){
-							toCrawl.push(link);
-						}
-					}
-					i++;
-				}
-			});
-			crawled[toCrawl[0]]=true;
-			brojiteracija++;
-			toCrawl.shift();
-			console.log(brojiteracija + "*************************");
-			console.log(toCrawl);
-			console.log(crawled);
-			if(brojiteracija == 3)
-				break;
-   	//	console.log(brojiteracija);
+    while(toCrawl.length>0)	{ 
+        var response=request('get',toCrawl[0]);
+        response=response.getBody();
+        var matches;			
+        linkscrape(toCrawl[0],response,function(links,$) {
+            var i=0;
+            while(i<links.length) {
+                var link=links[i].link;
 
-	}
+                if(link!==null && link.indexOf("https://")===-1) {
+                    if(!(crawled[link] == true || (toCrawl.indexOf(link)>-1) || link.indexOf('#') != -1)) {
+                        toCrawl.push(link);
+                    }
+                }
+                i++;
+            }
+        });
+        crawled[toCrawl[0]]=true;
+        brojiteracija++;
+        toCrawl.shift();
+        console.log(brojiteracija + "*************************");
+        console.log(toCrawl);
+        console.log(crawled);
+        if(brojiteracija == 3) {
+            break;
+        }
+        //	console.log(brojiteracija);
+    }
 }
 
 crawling(toCrawl[0]);
+
+
+// REST API
